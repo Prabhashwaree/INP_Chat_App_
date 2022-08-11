622cfc17c47464ff3e36ae191290cd02a2db8aa5
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-11 21:04:29 +0530

    Update userLoging fxml and ChatRoom fxml complite

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class LoginFormController {
+    public Button btnLogin;
+    public AnchorPane loginPane;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/ChatRoomForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) loginPane.getScene().getWindow();
+        window.setScene(new Scene(load));
+    }
 }
