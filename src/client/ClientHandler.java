6befb189df0c3e66ba5f22ad53fc5ac2f526a1d4
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-12 20:34:46 +0530

    client class creating

--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -1,10 +1,34 @@
 package client;
 
+import java.io.*;
 import java.net.Socket;
 
-public class ClientHandler{
+public class ClientHandler implements Runnable{
+
+    Socket socket = new Socket();
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+    BufferedReader bufferedReader;
+
+    public ClientHandler() {
+    }
+
+    public void initialize() {
+        new Thread(this::run).start();
+    }
 
 
-    
+    public void run() {
+        try {
 
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataInputStream = new DataInputStream(socket.getInputStream());
+
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
