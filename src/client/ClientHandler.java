ace5957cf13eabfc29da29fe797e96b1adba463f
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-13 04:24:21 +0530

    Re Creating Client class commit

--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -6,13 +6,51 @@ import java.util.ArrayList;
 
 public class ClientHandler extends Thread{
 
-    public static ArrayList<ClientHandler>client = new ArrayList<>();
+    private ArrayList<ClientHandler> clients;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    ObjectOutputStream objectOutputStream = null;
+    ObjectInputStream objectInputStream = null;
 
     public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+       try {
+           this.socket = socket;
+           this.clients = clients;
+           this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           this.writer = new PrintWriter(socket.getOutputStream(),true);
+       } catch (IOException exception) {
+           exception.printStackTrace();
+       }
     }
 
 
-
+    public void  runner(){
+        try{
+            String massage;
+            while ((massage = reader.readLine()) !=null){
+               if(massage.equals("Exits")){
+                   return;
+               }
+               for (ClientHandler clientHandler : clients){
+                   clientHandler.writer.println(massage);
+               }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+                writer.close();
+                socket.close();
+                System.out.println();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -21,39 +21,19 @@ public class Server {
         server.serverSocket();
     }*/
 
-    /*private void serverSocket() {
-        try{
-            while (!serverSocket.isClosed()) {
-                Socket socket = null;
 
-                socket = serverSocket.accept();
-
-                System.out.println("client has connected");
-                ClientHandler clientHandler = new ClientHandler(socket);
-                Thread thread = new Thread(clientHandler);
-                thread.start();
-            }
-        }catch (IOException exception){
-            exception.printStackTrace();
-        }
-    }*/
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
         Socket accept;
 
         while (true){
-            System.out.println("Waiting for Client ...");
             accept =serverSocket.accept();
             System.out.println("Connected");
-
-
             ClientHandler clientHandler = new ClientHandler(accept, clients);
             clients.add(clientHandler);
             clientHandler.start();
         }
-
     }
-
 
 }
