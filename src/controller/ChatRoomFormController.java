e05bb6d7dbf5e308b490d82f41e6b987373cbc93
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-13 04:51:56 +0530

    ChatRoom Controller Class Creating

--- src/controller/ChatRoomFormController.java
+++ src/controller/ChatRoomFormController.java
@@ -6,8 +6,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 
-public class ChatRoomFormController {
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatRoomFormController extends Thread {
     public TextArea txtArea;
     public Button btnCamara;
     public Button btnFile;
@@ -15,12 +20,32 @@ public class ChatRoomFormController {
     public TextField txtMessage;
     public AnchorPane chatRoomPane;
 
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket socket;
+    private FileChooser fileChooser;
+    private File filePath;
 
-    public static void main(String[] args) {
+    public void initialize() {
+
+        try{
+             socket = new Socket("localhost",8000);
+             System.out.println("socket ek awaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!");
+             reader =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             writer = new PrintWriter(socket.getOutputStream(),true);
+             this.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void txtAreaOnAction(MouseEvent mouseEvent) {
+
+
+
+        public void txtAreaOnAction(MouseEvent mouseEvent) {
     }
 
     public void btnCamaraOnAction(ActionEvent actionEvent) {
